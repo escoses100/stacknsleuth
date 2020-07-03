382bca33c012d335a5e3fcb77df6474bbaf56e8f
Scott Bennett <scottbennett@Scotts-MacBook-Pro.local>
2020-07-02 23:16:51 -0700

    updates

---
+++ content.js
@@ -0,0 +1,198 @@
+var maincontent = [{
+        "room": "1",
+        "roomtitle": "Room 1",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "2",
+        "roomtitle": "Room 2",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "3",
+        "roomtitle": "Room 3",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "4",
+        "roomtitle": "Room 4",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "5",
+        "roomtitle": "Room 5",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "6",
+        "roomtitle": "Room 6",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "7",
+        "roomtitle": "Room 7",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "8",
+        "roomtitle": "Room 8",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "9",
+        "roomtitle": "Room 9",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "10",
+        "roomtitle": "Room 10",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "11",
+        "roomtitle": "Room 11",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    },{
+        "room": "12",
+        "roomtitle": "Room 12",
+        "roomdescription": "Room description goes here.",
+        "roompuzzle": "Room puzzle here",
+        "roompuzzle_opt1":"option 1",
+        "roompuzzle_opt2":"option 2",
+        "roompuzzle_opt3":"option 3",
+        "roompuzzle_ans":"option 2",
+        "roompuzzle_ans_message":"Answer Message", 
+        "roompuzzle_ans_message":"Answer Message Wrong"
+    }
+]
+
+var mainpeople = [{
+    "person": "1",
+    "person_name": "Name One",
+    "person_description": "Person description goes here.",
+    "person_motive": "Person Motive Here",
+    "Personlead1":"Lead 1",
+    "Personlead2":"Lead 2",
+    "Personlead3":"Lead 3",
+    "Personalibi":"Alibi",
+    "Personguilty":"Guilty",
+    "Personguilty_message":"Guilty Message", 
+    "Personthankyou_message":"Person Thankyou"
+},{
+    "person": "2",
+    "person_name": "Name One",
+    "person_description": "Person description goes here.",
+    "person_motive": "Person Motive Here",
+    "Personlead1":"Lead 1",
+    "Personlead2":"Lead 2",
+    "Personlead3":"Lead 3",
+    "Personalibi":"Alibi",
+    "Personguilty":"Guilty",
+    "Personguilty_message":"Guilty Message", 
+    "Personthankyou_message":"Person Thankyou"
+},{
+    "person": "3",
+    "person_name": "Name One",
+    "person_description": "Person description goes here.",
+    "person_motive": "Person Motive Here",
+    "Personlead1":"Lead 1",
+    "Personlead2":"Lead 2",
+    "Personlead3":"Lead 3",
+    "Personalibi":"Alibi",
+    "Personguilty":"Guilty",
+    "Personguilty_message":"Guilty Message", 
+    "Personthankyou_message":"Person Thankyou"
+},{
+    "person": "4",
+    "person_name": "Name One",
+    "person_description": "Person description goes here.",
+    "person_motive": "Person Motive Here",
+    "Personlead1":"Lead 1",
+    "Personlead2":"Lead 2",
+    "Personlead3":"Lead 3",
+    "Personalibi":"Alibi",
+    "Personguilty":"Guilty",
+    "Personguilty_message":"Guilty Message", 
+    "Personthankyou_message":"Person Thankyou"
+}]
+
+var beginandend = [{
+    "intro": "1",
+    "introstatement": "Introduction to the story.",
+    "introchallenge": "This is the challenge.",
+    "endchallenge": "The challenge was.",
+    "challengeoption1":"Option 1",
+    "challengeoption2":"Option 2",
+    "challengeoption3":"Option 3",
+    "challengeoption4":"Option 4",
+    "challengeanswer":"Option 3",
+    "end_correct_message":"Correct Message", 
+    "end_incorrect_message":"Incorrect Message"
+}]
